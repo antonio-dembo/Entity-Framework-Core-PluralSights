77c5f18055ba7f4b6fe67e32a0e9bfaab5bc617a
beboantonio <beboantonio1@hotmail.com>
2021-10-28 10:06:00 -0300

    a sneek pick at writing and reading data

--- SamuraiApp/SamuraiApp.Data/SamuraiContext.cs
+++ SamuraiApp/SamuraiApp.Data/SamuraiContext.cs
@@ -11,7 +11,7 @@ namespace SamuraiApp.Data
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(
-                "Data Source= (localdb)\\MSSQLLocalDB; Initial Catalog=SamuraiAppData");
+                "Data Source= (localdb)\\MSSQLLocalDB; Initial Catalog=SamuraiAppData; Integrated Security=true");
         }
     }
 }
--- SamuraiApp/SamuraiApp.UI/Program.cs
+++ SamuraiApp/SamuraiApp.UI/Program.cs
@@ -1,12 +1,38 @@
-﻿using System;
+﻿using SamuraiApp.Data;
+using SamuraiApp.Domain;
+using System;
+using System.Linq;
 
 namespace SamuraiApp.UI
 {
     class Program
     {
+        private static SamuraiContext _context = new SamuraiContext();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            _context.Database.EnsureCreated();
+            GetSamurai("Before Add");
+            AddSamurai();
+            GetSamurai("After Add");
+            Console.Write("Press any key ...");
+            Console.ReadKey();
+        }
+
+        public static void AddSamurai()
+        {
+            var samurai = new Samurai { Name = "Sampson" };
+            _context.Samurais.Add(samurai);
+            _context.SaveChanges();
+        }
+
+        public static void GetSamurai(string text)
+        {
+            var samurais = _context.Samurais.ToList();
+            Console.WriteLine($"{text}: samurai count is {samurais.Count}.");
+            foreach (var samurai in samurais)
+            {
+                Console.WriteLine(samurai.Name);
+            }
         }
     }
 }
